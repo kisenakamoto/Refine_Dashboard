f91d13defea0bc2cbff656835ac54cff0ea4afd5
Gem Ryan <gembencito@gmail.com>
2023-04-21 21:04:14 +0800

    All properties, sorting, filtering, paginate

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -11,23 +11,23 @@ const getAllUsers = async (req, res) => {
 };
 
 const createUser = async (req, res) => {
-//   try {
-//     const { name, email, avatar } = req.body;
+  try {
+    const { name, email, avatar } = req.body;
   
-//     const userExists = await User.findOne({ email });
+    const userExists = await User.findOne({ email });
   
-//     if(userExists) return res.status(200).json(userExists);
+    if(userExists) return res.status(200).json(userExists);
   
-//     const newUser = await User.create({
-//       name,
-//       email,
-//       avatar
-//     })
+    const newUser = await User.create({
+      name,
+      email,
+      avatar
+    })
   
-//     res.status(200).json(newUser);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message })
-//   }
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 };
 
 const getUserInfoByID = async (req, res) => {
