4db8e9b5241ca71fe8c0b43e6fee3f966a1e3924
Gem Ryan <gembencito@gmail.com>
2023-04-28 20:29:27 +0800

    Agent and Agent Profile

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,13 +1,13 @@
 import User from '../mongodb/models/user.js';
 
 const getAllUsers = async (req, res) => {
-//   try {
-//     const users = await User.find({}).limit(req.query._end);
+  try {
+    const users = await User.find({}).limit(req.query._end);
 
-//     res.status(200).json(users);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message })
-//   }
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 };
 
 const createUser = async (req, res) => {
@@ -31,19 +31,19 @@ const createUser = async (req, res) => {
 };
 
 const getUserInfoByID = async (req, res) => {
-//   try {
-//     const { id } = req.params;
+  try {
+    const { id } = req.params;
     
-//     const user = await User.findOne({ _id: id}).populate('allProperties');
+    const user = await User.findOne({ _id: id}).populate('allProperties');
     
-//     if(user) {
-//       res.status(200).json(user)
-//     } else {
-//       res.status(404).json({ message: 'User not found' });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ message: error.message })
-//   }
+    if(user) {
+      res.status(200).json(user)
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }; 
 
 export {
